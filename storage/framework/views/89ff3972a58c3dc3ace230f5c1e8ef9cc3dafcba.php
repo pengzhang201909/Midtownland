798ccff1325e136edae6d31e993ab0e97b9888e4
Peng Zhang <55248789+pengzhang201909@users.noreply.github.com>
2020-04-19 09:30:08 -0400

    Add files via upload

---
+++ storage/framework/views/89ff3972a58c3dc3ace230f5c1e8ef9cc3dafcba.php
@@ -0,0 +1,31 @@
+<?php $__env->startSection('content'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="p-3" >
+                <img src="/svg/midtownlandlogo.svg">
+            </div>
+            <div class="p-3">
+                <div><h1>Midtownland</h1></div>
+                <div>has excellent properties in Central Hudson Valley</div>
+                <div>
+                    <div><strong>10</strong> posts</div>
+                    <div><strong>2</strong> followers</div>
+                    <div><strong>1</strong> following</div>
+            </div>
+        </div>
+        <div class="row pt-4">
+            <div  class="col-6">
+                <img src="/svg/webbhortonhouse.svg" class="w-3">
+            </div>
+            <div class="col-6">
+                <img src="/svg/townlogo.svg" class="w-3">
+            </div>
+            <div class="col-6">
+                <img src="/svg/highschool.svg" class="w-3">
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laravel\laravel\midtownland\resources\views/home.blade.php ENDPATH**/ ?>
